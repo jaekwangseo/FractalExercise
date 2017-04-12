b8ceb26b37b000679b77963cb30ed3113d8533ea
Jaekwang Seo <jaekwang.seo@gmail.com>
2017-04-12 07:38:08 -0400

    before refactoring for performance

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const parse = require('csv-parse');
-var syncParse = require('csv-parse/lib/sync');
 const fs = require('fs');
-const readline = require('readline');
 
+let global_movies = undefined;
 
 const parse_data = filePath => {
+
+  if (global_movies && global_movies.length > 0) {
+    return global_movies;
+  }
 
   const lines = fs.readFileSync(filePath).toString().split('\n');
   const linesArray = lines.filter(line => {
@@ -14,6 +16,7 @@ const parse_data = filePath => {
     // Convert string into an array
     return line.replace('\r', '').split(',');
   })
+  // console.log('this is running');
 
   // First line contains labels for each column
   const keys = linesArray.shift();
@@ -27,6 +30,7 @@ const parse_data = filePath => {
     return obj;
   })
 
+  global_movies = linesObj;
   return linesObj;
 }
 
@@ -50,9 +54,8 @@ const get_movies = path => {
  */
 const get_movie = id => {
 
-  const movies = get_movies('movie_metadata_sample.csv');
+  const movies = get_movies('movie_metadata.csv');
   const filtered = movies.filter(movie => {
-    //console.log('movie', movie)
     return parse_imdb_id(movie.movie_imdb_link) === id
   })
 
@@ -61,8 +64,6 @@ const get_movie = id => {
   }
   return null;
 }
-// let a = get_movie('tt0499549')
-// console.log(a)
 
 
 /**
@@ -87,8 +88,6 @@ const get_score = id => {
   const sciFiFantasyBonus = sciFiOrFantasy(movie.genres) ? 1.5 : 0;
   return imdb_score * 2.3 - movie_year / 2000 + sciFiFantasyBonus;
 }
-// let b = get_score('tt0499549');
-// console.log(b);
 
 
 const containsFavoriteGenre = (movie_genres, user_genres) => {
@@ -96,7 +95,7 @@ const containsFavoriteGenre = (movie_genres, user_genres) => {
     return acc || movie_genres.includes(genre);
   }, false);
 }
-// console.log(favoriteGenre('Action|Sci-Fi', 'Drama'));
+
 
 /**
  *
@@ -115,44 +114,27 @@ const get_user_score = (id, user) => {
                     + (containsFavoriteGenre(movie.genres, user.favorite_genres) ? 3.5 : 0);
   return userScore;
 }
-// const testUser = {
-//   id: 12,
-//   likes_old_movies: true,
-//   favorite_genres: 'Action|Sci-Fi|Drama'
-// };
-// const userScore = get_user_score('tt0499549', testUser)
-// console.log(userScore);
 
 
 
 
 const get_top_10_movies_for_user = user => {
-  let movies = get_movies('movie_metadata_sample.csv');
-  // console.log(movies);
-
+  let movies = get_movies('movie_metadata.csv');
+  // console.log('after get_movies');
   movies = movies.map(movie => {
+    // console.log('movie:', movie)
     movie.user_specific_score = get_user_score(parse_imdb_id(movie.movie_imdb_link), user);
     return movie;
   });
-  // console.log(movies);
 
   return movies.sort((a, b) => {
     return b.user_specific_score - a.user_specific_score;
   }).slice(0, 10);
 }
-const testUser2 = {
+const user = {
   id: 12,
   likes_old_movies: true,
   favorite_genres: 'Action|Sci-Fi|Drama'
 };
-const top10Result = get_top_10_movies_for_user(testUser2);
+const top10Result = get_top_10_movies_for_user(user);
 console.log(top10Result);
-
-
-
-
-// console.log(get_top_10_movies_for_user(testUser));
-
-//TEST
-// const a = parse_data('movie_metadata_sample.csv');
-// console.log(a);
